06ee65932313e0cb04281a32c97cbad6834553e7
Linchaker <linchaker@gmail.com>
2020-09-01 13:13:48 +0300

    add phpcs config change test class

--- src/SimpleClass.php
+++ src/SimpleClass.php
@@ -1,8 +1,22 @@
 <?php
-declare(strict_types=1);
-namespace Linchaker\Travis;
-class SimpleClass {
 
+declare(strict_types=1);
+
+namespace Linchaker\Travis;
+
+/**
+ * Class SimpleClass
+ *
+ * @package Linchaker\Travis
+ */
+class SimpleClass
+{
+
+    /**
+     * main page
+     *
+     * @return void
+     */
     public function index()
     {
         echo 'Hello';
